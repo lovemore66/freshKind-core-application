6e78484e751116cd038f61ae636046bacd0ceaf0
lovemore66 <sinkondeluvmo11@gmail.com>
2025-04-20 22:15:04 +0200

    Ignore .history folder

--- .history/frontend/src/components/Theme/theme_20250417064949.ts
+++
@@ -1,8 +0,0 @@
-import { extendTheme } from "@chakra-ui/react"
-
-export const theme = extendTheme({
-  fonts: {
-    heading: `'Figtree', sans-serif`,
-    body: `'Figtree', sans-serif`,
-  },
-})
--- .history/frontend/src/store/hooks/hooks_20250416190106.ts
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from '../store'; // 👈 adjust if store is in a different folder
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
